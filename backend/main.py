e47b56a8448e9ca991e8eb0ea6204cb0a5709864
black <joseb.twelve@gmail.com>
2025-11-20 16:25:28 -0500

    fix: fixed bug on backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from conf.database import engine, Base
 from routes import cart_route, item_route
 
 app = FastAPI()
+
+origins = ("http://localhost", "http://localhost:8000", "http://localhost:5173")
+
+allow_methods = ("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+allow_headers = ("Authorization", "Content-Type", "Accept", "X-Requested-With")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=allow_methods,
+    allow_headers=allow_headers,
+)
 
 
 # Create tables on startup
--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -2,11 +2,31 @@ import Navbar from "@/components/navbar"
 import { useCartStore } from "@/store/cart_store"
 import CruiseIcon from "@/components/icons/cruise"
 import type { Item } from "@/domain/types/item_type"
+import type { Cart } from "@/domain/types/cart_type"
 
+const backend_url = "http://localhost:8000/cart"
 const Cart = () => {
   const { data: cart_items, delete_item: delete_item_from_store } = useCartStore((state) => state)
   const delete_item = ({item}: {item: Item}) => {
     delete_item_from_store(item)
+  }
+  const save_cart = (cart: Cart) => {
+    const clean_cart = {
+      id: cart.id,
+      items: cart.items.map((e) => e.id)
+    }
+    fetch(
+      backend_url,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(clean_cart)
+      }
+    ).then((response) => {response.json()})
+    .then(() => alert("Cart guardado correctamente"))
+    .catch((error) => alert(`Error: ${error}`))
   }
   return <main className="mt-12 px-2 sm:px-0 w-screen">
     <Navbar />
@@ -16,6 +36,9 @@ const Cart = () => {
         <p className="font-medium">Total:</p>
         <p className="">${Math.round(cart_items.total * 100) / 100}</p>
       </div>
+    </div>
+    <div className="w-full sm:pr-40 pr-10 flex justify-end mb-3 place-content-center">
+      <button className="bg-blue-800 text-white px-2 font-medium rounded py-1 hover:bg-blue-600 hover:scale-101 transition-all cursor-pointer" onClick={() => save_cart(cart_items)}>Guardar</button>
     </div>
     <ul className="flex gap-1 flex-col w-full place-items-center">
       {cart_items.items.map((e, idx) => (
